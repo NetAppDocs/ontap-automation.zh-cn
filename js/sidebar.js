dba5ef7cc39d7a443143587c616fcc14113b48c3
akseldavis <33839009+akseldavis@users.noreply.github.com>
2022-07-15 22:36:26 +0000

    Deploying to gh-pages from  @ 2f845ef8f1c7a6d00de2c61eb28e8f8c7244f232 🚀

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+window.addEventListener("load", () => {
     var sidebarDiv = document.getElementById("sidebar-height");
     var productNavDiv = document.getElementById("product-nav");
     if (sidebarDiv.contains(productNavDiv)) {
@@ -6,4 +6,10 @@ window.onload = () => {
     } else {
         document.getElementById("mysidebardiv").classList.add("with-padding");
     }
-}
+
+    let pdfContainer = document.getElementById("toggleContainerPdf");
+    let pdfPageLink = document.getElementById("pdf-remove");
+    if (pdfContainer.contains(pdfPageLink)) {
+        pdfPageLink.remove();
+    }
+});
